4f09617e42dc9480d104d378808674524601518e
Leesomoney <127239280+sidemix@users.noreply.github.com>
2025-11-25 13:46:11 -0500

    3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app.web import app
-import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
